da58256ecf97abb8f5f7c320d14675e12f02a976
Philip Doldo <philipmdoldo@gmail.com>
2026-03-21 18:16:20 -0400

    added Block class and normalization logic, fixed some comments

--- model.py
+++ model.py
@@ -44,10 +44,11 @@ class MLP(nn.Module):
 class TimeAndClassEmbedding(nn.Module):
 
     def __init__(self, d, hidden_dim=None, num_classes=10, base=10000):
+        """
+        `d` is the embedding dimension for time/class
+        `hidden_dim` is the optional hidden dimension in the MLP, defaults to 4*d
+        """
         super().__init__()
-        self.d = d
-        self.hidden_dim = hidden_dim
-
         self.time_embedding = SinusoidalEmbedding(d=d, base=base)
         self.class_embedding = nn.Embedding(num_classes+1, d) # +1 for null class for classifier-free guidance (CFG)
         self.mlp = MLP(input_dim=d, hidden_dim=hidden_dim)
@@ -66,6 +67,44 @@ class TimeAndClassEmbedding(nn.Module):
         return embeddings # shape (B, d)
 
 
+class GroupNorm32(nn.GroupNorm):
+    """
+    Taken from https://github.com/KellyYutongHe/cmu-10799-diffusion/blob/main/src/models/blocks.py#L69
+
+    GroupNorm is initialized with `num_groups`, `num_channels`, `eps` (default 1e-5), and `affine` (default True)
+
+    GroupNorm takes a tensor with shape e.g. (B, C, H, W) as input (only the first two dimensions matter) where C must
+    be equal to `num_channels` and C/num_groups (the group size) must be an integer. For a fixed batch and a group of channels,
+    there are (C/num_group)*H*W values and the sample mean mu(b, g) and variance sigma^2(b, g) are computed where b is the batch
+    and g is the group of channels. Every entry in the group is standardized according to these values (i.e. by doing
+    (x-mean)/(sqrt(variance + eps), roughly speaking). This is performed across all batches and groups. When `affine` is true,
+    there are two learnable vectors of shape (C,), one which scales the result and one which shifts it, where the scale and shift
+    are potentially different for each channel. The scale is initialized to all 1's and the shift is initialized to all 0's. 
+
+    GroupNorm32 does GroupNorm but with float32 precision since it is claimed that diffusion models can be sensitive to numerical
+    precision during normalization. It casts to float32 and then back to the original precision. It would be interesting to test
+    how much this actually matters.
+    """
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return super().forward(x.float()).type(x.dtype)
+
+
+def rmsnorm(x):
+    """
+    `x` has shape (B, C, H, W) and the RMS (which is just 2-norm scaled by 1/sqrt(d) in R^d) is computed for every vector of channels
+    No learnable parameters. I want to try rmsnorm since I used it in language models. 
+    """
+    rms = x.pow(2).mean(dim=1, keepdim=True).sqrt() # shape (B, 1, H, W)
+    return (x / (rms + 1e-8))
+
+
+def rmsnorm32(x):
+    """
+    rmsnorm but now with float32 precision just in case that ends up being important for stability
+    """
+    rms = x.float().pow(2).mean(dim=1, keepdim=True).sqrt()
+    return (x / (rms + 1e-8)).type(x.dtype)
 
 
 
@@ -73,8 +112,12 @@ class Downsample(nn.Module):
     """
     Downsampling layer that halves the spatial dimensions (height and width)
 
-    Input has shape (B, H, W, C) where C is the number of input channels and the output should have shape (B, H/2, W/2, C)
-    where H and W are assumed to be even
+    Input has shape (B, C, H, W) where C is the number of input channels and the output should have shape (B, C, ceil(H/2), ceil(W/2))
+    Naively, we don't assume that H and W are even because many images have spatial dimensions that are not even divisible by 4 or 8 and thus
+    we need to accept that downsampling followed by upsampling could actually strictly increase the spatial dimensions and thus we need
+    to be careful with our residual connections as they might not have the same spatial dimensions --- however, it seems like in practice
+    people actually just resize their image spatial dimensions in a preprocessing step to the nearest power of 2 to avoid this issue. Because
+    of this, I will assert that the input spatial dimensions must be even.
 
     Originally, I was going to use nn.MaxPool2d(kernel_size=2, stride=2) which considers a 2x2 grid (starting in the top-left corner
     with no padding, so that the entire 2x2 grid is fully contained within the image) and consolidates ("pools") the 4 values in the
@@ -85,9 +128,9 @@ class Downsample(nn.Module):
     However, instead of doing 2x2 max pooling with a stride of 2, you can do a 3x3 convolution with a stride of 2, a padding of 1, and
     using the same number of filters as there are input channels (so the output has the same number of channels that the input has) and
     this will also reduce the spatial dimensions by a factor of 2 but now instead of arbitrarily restricting the model to do max pooling,
-    we are effectively allowing the model to learn what kind of (potentially complicated) pooling to do. Each filter has shape (3,3,C) and
+    we are effectively allowing the model to learn what kind of (potentially complicated) pooling to do. Each filter has shape (C, 3, 3) and
     we are using C filters, so this comes at the cost of adding 9*C^2 parameters, but it might learn a smarter pooling approach. I got this
-    idea from https://github.com/KellyYutongHe/cmu-10799-diffusion/blob/main/src/models/blocks.py#L254 where I also took some other minor
+    idea from https://github.com/KellyYutongHe/cmu-10799-diffusion/blob/main/src/models/blocks.py#L254 which I also took some other minor
     inspirations from here and there.
     """
 
@@ -103,12 +146,12 @@ class Downsample(nn.Module):
 
     def forward(self, x):
         """
-        `x` has shape (B, H, W, C) where B is batch size, H is height, W is width, and C is channels
+        `x` has shape (B, C, H, W) where B is batch size, H is height, W is width, and C is channels
         output will have shape
         """
-        B, H, W, C = x.shape
+        B, C, H, W = x.shape
         if H % 2 != 0 or W % 2 != 0:
-            raise ValueError(f"{x.shape=}, height and width should be even but {H=}, {W=}")
+            raise ValueError(f"{x.shape=}, {H=}, {W=}, but both must be even to avoid spatial dimension mismatches when using the residual connections in the U-Net")
         if C != self.channels:
             raise ValueError(f"{x.shape=}, {C=}, {self.channels=}")
         return self.conv(x)
@@ -118,9 +161,9 @@ class Upsample(nn.Module):
     """
     Upsampling layer that doubles the spatial dimensions (height and width)
 
-    Input has shape (B, H, W, C) where C is the number of input channels and the output should have shape (B, 2*H, 2*W, C)
+    Input has shape (B, C, H, W) where C is the number of input channels and the output should have shape (B, C, 2*H, 2*W)
 
-    Originally I was going to do a "transposed convolution" which uses C filters of of shape (2, 2, C) and for each pixel value
+    Originally I was going to do a "transposed convolution" which uses C filters of of shape (C, 2, 2) and for each pixel value
     in a given channel, the pixel value p scales all 4 values in the 2x2 grid for the current value which results in a 2x2 for
     each channel, all C of these 2x2 matrices are summed to obtain a single 2x2 matrix corresponding to this filter. This is then
     performed for all C filters (since we chose to use C filters, the number of output channels matches the number of input channels).
@@ -135,7 +178,7 @@ class Upsample(nn.Module):
     a convolution to its output, e.g. `nn.Conv2d(channels, channels, kernel_size=3, padding=1)`. The choice to use a 3x3 convolution 
     seems like it is kind of arbitrary, I am just copying this from https://github.com/KellyYutongHe/cmu-10799-diffusion/blob/main/src/models/blocks.py#L286
     
-    So what does `F.interpolate(x, scale_factor=2, mode='nearest')` actually do? Suppose x has shape (B, H, W, C), consider a single
+    So what does `F.interpolate(x, scale_factor=2, mode='nearest')` actually do? Suppose x has shape (B, C, H, W), consider a single
     H-by-W grid for a fixed batch and channel. We define H' = round(scale_factor * H) and W' = round(scale_factor * W) (I don't actually
     know how rounding is done, but it isn't important), so the height of our grid went from having indices [0, 1, 2, ..., H-1] to having
     indices [0, 1, 2, ..., H'-1] and in our case H' = 2*H. If we have some height index h' in our new grid, we determine its value by 
@@ -158,11 +201,78 @@ class Upsample(nn.Module):
         self.conv = nn.Conv2d(channels, channels, kernel_size=3, padding=1)
     
     def forward(self, x):
-        B, H, W, C = x.shape
+        B, C, H, W = x.shape
         if C != self.channels:
             raise ValueError(f"{x.shape=}, {C=}, {self.channels=}")
         x = F.interpolate(x, scale_factor=2, mode='nearest')
         return self.conv(x)
+
+
+class Block(nn.Module):
+    """
+    Partially inspired by https://github.com/KellyYutongHe/cmu-10799-diffusion/blob/main/src/models/blocks.py#L85
+    I'm choosing not to use FiLM for now to keep things simple, can always ablate later
+
+    We use 3x3 Conv w/ padding=1 to preserve spatial dimensions (this avoids the cropping that was done in the original U-Net paper)
+
+    z -> Nonlinearity -> Linear -> z'
+    x -> Norm -> Nonlinearity -> Conv -> add z' -> Norm -> Nonlinearity -> Dropout -> Conv -> Residual Connection (add x or Conv(x), use the latter if in_channels != out_channels)  
+    
+    input of shape (B, in_channels, H, W) gets mapped to output of shape (B, out_channels, H, W)
+    """
+
+    def __init__(self, in_channels: int, out_channels: int, d: int, dropout: float = 0.0):
+        """
+        `in_channels` is the number of channels in the input
+        `out_channels` is the number of channels in the output
+        `d` is the time/class embedding dimension
+        `dropout` is the dropout probability
+        """
+        super().__init__()
+        self.in_channels = in_channels
+
+        self.W = nn.Linear(d, out_channels) # projection for time/class embeddings for this specific layer
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.dropout = nn.Dropout(dropout)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+
+        # Residual Connection
+        if in_channels == out_channels:
+            self.skip_connection = nn.Identity()
+        else:
+            self.skip_connection = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1) # in this case, the point of the convolution is just to adjust the number of channels so we can add it to the output at the end
+
+    def forward(self, x, z):
+        """
+        `x` has shape (B, C, H, W) is the primary input tensor
+        `z` has shape (B, d) and represents the time/class embedding vectors, d is the dimension of these embeddings
+        """
+        B, C, H, W = x.shape
+        if C != self.in_channels:
+            raise ValueError(f"{x.shape=}, {C=}, {self.in_channels=}")
+
+        # First Conv Block: Norm -> Nonlinearity -> Conv
+        h = rmsnorm(x)          # (B, C, H, W)      intentionally keep `x` unmodified for residual connection at the end
+        h = F.relu(h).square()  # (B, C, H, W)
+        h = self.conv1(h)       # (B, out_channels, H, W)
+
+        # apply nonlinearity and then project time/class embeddings 
+        z = F.relu(z).square()  # (B, d)
+        z = self.W(z)           # (B, out_channels)
+        z = z[:, :, None, None] # (B, out_channels, 1, 1)
+
+        # Add context from time/class embeddings 
+        h = h + z               # (B, out_channels, H, W)
+
+        # Second Conv Block: 
+        h = rmsnorm(h)          # (B, out_channels, H, W)
+        h = F.relu(h).square()  # (B, out_channels, H, W)
+        h = self.dropout(h)     # (B, out_channels, H, W)
+        h = self.conv2(h)       # (B, out_channels, H, W)
+
+        # Residual Connection
+        return h + self.skip_connection(x) # (B, out_channels, H, W)
 
 
 class UNet(nn.Module):
